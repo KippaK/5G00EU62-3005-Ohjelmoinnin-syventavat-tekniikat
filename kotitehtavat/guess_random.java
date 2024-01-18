d214b9fcf42878b65cda3806833aa404054d4922
KippaK <antti.venetjoki@gmail.com>
2024-01-18 08:55:19 +0200

    now matches instructions

--- kotitehtavat/guess_random.java
+++ kotitehtavat/guess_random.java
@@ -10,7 +10,7 @@ public class guess_random {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int correct = rand.nextInt(99) + 1;
+		int correct = rand.nextInt(100) + 1;
 		for (int i = 0; i < 7; i++) {
 			int guess = guess();
 			if (guess == correct) {
